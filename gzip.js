f18f771e73c26fa1f581083ab3c28093fd01c261
gongdingfei <156821840@qq.com>
2020-04-13 23:14:32 +0800

    gzip测试

---
+++ gzip.js
@@ -0,0 +1,36 @@
+const zlib = require('zlib');
+const http = require('http');
+const fs = require('fs');
+const { pipeline } = require('stream');
+http.createServer((request, response) => {
+  const raw = fs.createReadStream('index.html');
+  let acceptEncoding = request.headers['accept-encoding'];
+  if (!acceptEncoding) {
+    acceptEncoding = '';
+  }
+
+  const onError = (err) => {
+    if (err) {
+      // 如果发生错误，则我们将会无能为力，
+      // 因为服务器已经发送了 200 响应码，
+      // 并且已经向客户端发送了一些数据。
+      // 我们能做的最好就是立即终止响应并记录错误。
+      response.end();
+      console.error('发生错误:', err);
+    }
+  };
+
+  if (/\bgzip\b/.test(acceptEncoding)) {
+    response.writeHead(200, { 'Content-Encoding': 'deflate' });
+    pipeline(raw, zlib.createDeflate(), response, onError);
+  } else if (/\bdeflate\b/.test(acceptEncoding)) {
+    response.writeHead(200, { 'Content-Encoding': 'gzip' });
+    pipeline(raw, zlib.createGzip(), response, onError);
+  } else if (/\bbr\b/.test(acceptEncoding)) {
+    response.writeHead(200, { 'Content-Encoding': 'br' });
+    pipeline(raw, zlib.createBrotliCompress(), response, onError);
+  } else {
+    response.writeHead(200, {});
+    pipeline(raw, response, onError);
+  }
+}).listen(1337);
